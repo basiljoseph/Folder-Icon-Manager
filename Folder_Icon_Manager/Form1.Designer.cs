5e25df2ad3fcd2ee3e42e41c1572da194075177c
Basil Joseph <basil.joseph@outlook.com>
2017-12-31 22:08:25 +0530

    Initial Commit

---
+++ Folder_Icon_Manager/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Folder_Icon_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox_icon = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button_open_folder = new System.Windows.Forms.Button();
+            this.button_change_icon = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_icon_size = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_image_size = new System.Windows.Forms.Label();
+            this.groupBox_dimensions = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_file_size = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_icon)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox_dimensions.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox_icon
+            // 
+            this.pictureBox_icon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_icon.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox_icon.Name = "pictureBox_icon";
+            this.pictureBox_icon.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox_icon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_icon.TabIndex = 0;
+            this.pictureBox_icon.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 289);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(414, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(81, 17);
+            this.toolStripStatusLabel1.Text = "Select a folder";
+            // 
+            // button_open_folder
+            // 
+            this.button_open_folder.Location = new System.Drawing.Point(274, 27);
+            this.button_open_folder.Name = "button_open_folder";
+            this.button_open_folder.Size = new System.Drawing.Size(128, 39);
+            this.button_open_folder.TabIndex = 2;
+            this.button_open_folder.Text = "Select Icon";
+            this.button_open_folder.UseVisualStyleBackColor = true;
+            this.button_open_folder.Click += new System.EventHandler(this.button_open_folder_Click);
+            // 
+            // button_change_icon
+            // 
+            this.button_change_icon.Enabled = false;
+            this.button_change_icon.Location = new System.Drawing.Point(273, 244);
+            this.button_change_icon.Name = "button_change_icon";
+            this.button_change_icon.Size = new System.Drawing.Size(129, 39);
+            this.button_change_icon.TabIndex = 2;
+            this.button_change_icon.Text = "Change Icon";
+            this.button_change_icon.UseVisualStyleBackColor = true;
+            this.button_change_icon.Click += new System.EventHandler(this.button_change_icon_Click);
+            // 
+            // ofd
+            // 
+            this.ofd.DefaultExt = "ico";
+            this.ofd.Filter = "Icon files (*.ico)|*.ico|All files (*.*)|*.*";
+            this.ofd.RestoreDirectory = true;
+            this.ofd.Title = "Select the icon file";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Icon";
+            // 
+            // label_icon_size
+            // 
+            this.label_icon_size.AutoSize = true;
+            this.label_icon_size.Location = new System.Drawing.Point(48, 39);
+            this.label_icon_size.Name = "label_icon_size";
+            this.label_icon_size.Size = new System.Drawing.Size(28, 13);
+            this.label_icon_size.TabIndex = 4;
+            this.label_icon_size.Text = ": NA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Image";
+            // 
+            // label_image_size
+            // 
+            this.label_image_size.AutoSize = true;
+            this.label_image_size.Location = new System.Drawing.Point(48, 16);
+            this.label_image_size.Name = "label_image_size";
+            this.label_image_size.Size = new System.Drawing.Size(28, 13);
+            this.label_image_size.TabIndex = 4;
+            this.label_image_size.Text = ": NA";
+            // 
+            // groupBox_dimensions
+            // 
+            this.groupBox_dimensions.Controls.Add(this.label2);
+            this.groupBox_dimensions.Controls.Add(this.label_image_size);
+            this.groupBox_dimensions.Controls.Add(this.label1);
+            this.groupBox_dimensions.Controls.Add(this.label_icon_size);
+            this.groupBox_dimensions.Location = new System.Drawing.Point(274, 72);
+            this.groupBox_dimensions.Name = "groupBox_dimensions";
+            this.groupBox_dimensions.Size = new System.Drawing.Size(128, 63);
+            this.groupBox_dimensions.TabIndex = 5;
+            this.groupBox_dimensions.TabStop = false;
+            this.groupBox_dimensions.Text = "Dimensions";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label_file_size);
+            this.groupBox1.Location = new System.Drawing.Point(274, 141);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(128, 63);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Properties";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Size";
+            // 
+            // label_file_size
+            // 
+            this.label_file_size.AutoSize = true;
+            this.label_file_size.Location = new System.Drawing.Point(48, 16);
+            this.label_file_size.Name = "label_file_size";
+            this.label_file_size.Size = new System.Drawing.Size(28, 13);
+            this.label_file_size.TabIndex = 4;
+            this.label_file_size.Text = ": NA";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(414, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 311);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox_dimensions);
+            this.Controls.Add(this.button_change_icon);
+            this.Controls.Add(this.button_open_folder);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox_icon);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(430, 350);
+            this.Name = "Form1";
+            this.Text = "Folder Icon Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_icon)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox_dimensions.ResumeLayout(false);
+            this.groupBox_dimensions.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_icon;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button_open_folder;
+        private System.Windows.Forms.Button button_change_icon;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_icon_size;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_image_size;
+        private System.Windows.Forms.GroupBox groupBox_dimensions;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_file_size;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Folder_Icon_Manager/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace Folder_Icon_Manager
+{
+    public partial class Form1 : Form
+    {
+        private string iconFile = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void reset()
+        {
+            button_change_icon.Enabled = false;
+            toolStripStatusLabel1.Text = "Select Folder Icon";
+            statusStrip1.Refresh();
+        }
+
+        private void show_icon()
+        {
+            Image image = Image.FromFile(iconFile);
+            pictureBox_icon.Image = image;
+            label_image_size.Text = ": " + image.Width.ToString() + " x " + image.Height.ToString();
+
+            Icon icon = new Icon(iconFile);
+            label_icon_size.Text = ": " + icon.Size.Width.ToString() + " x " + icon.Size.Height.ToString();
+
+            FileInfo file = new FileInfo(iconFile);
+            long length = file.Length;
+            if (length < 1024)
+                label_file_size.Text = ": " + length + " bytes";
+            else if (length < 1024 * 1024)
+                label_file_size.Text = ": " + (length / 1024.0).ToString("0.00") + " KB";
+            else
+                label_file_size.Text = ": " + (length / (1024.0 * 1024.0)).ToString("0.00") + " MB";
+        }
+
+        private void button_open_folder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /* Reset everything */
+                reset();
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    iconFile = ofd.FileName;
+                    show_icon();
+                    button_change_icon.Enabled = true;
+                    toolStripStatusLabel1.Text = "Loaded " + Path.GetFileNameWithoutExtension(iconFile); ;
+                }
+            }
+            catch (Exception exp)
+            {
+                toolStripStatusLabel1.Text = "Occured" + exp.GetType().ToString();
+                MessageBox.Show(exp.Message);
+            }
+            finally
+            {
+                statusStrip1.Refresh();
+            }
+        }
+
+        private void set_file_permissions(string path, FileAttributes attributes)
+        {
+            if (File.Exists(path))
+            {
+                File.SetAttributes(path, attributes);
+            }
+        }
+
+        private void set_directory_permissions(string path, FileAttributes attributes)
+        {
+            /* First chech whether we have write permission in the folder */
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            dirInfo.Attributes = attributes;
+        }
+
+        private void create_desktop_ini(string path, string iconName)
+        {
+            string desktopIniPath = path + @"\desktop.ini";
+            set_file_permissions(desktopIniPath, FileAttributes.Normal);
+
+            string iniContent = "[.ShellClassInfo]\r\nIconResource=";
+            iniContent += iconName;
+            iniContent += ",0\r\n";
+            File.WriteAllText(desktopIniPath, iniContent);
+
+            set_file_permissions(desktopIniPath, FileAttributes.System | FileAttributes.Hidden);
+        }
+
+        private void button_change_icon_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = Path.GetDirectoryName(iconFile);
+                string iconName = Path.GetFileName(iconFile);
+
+                set_directory_permissions(path, FileAttributes.Normal);
+                create_desktop_ini(path, iconName);
+                set_directory_permissions(path, FileAttributes.ReadOnly);
+                toolStripStatusLabel1.Text = "Icon Set for " + path;
+            }
+            catch (Exception exp)
+            {
+                toolStripStatusLabel1.Text = "Occured" + exp.GetType().ToString();
+                MessageBox.Show(exp.Message);
+            }
+            finally
+            {
+                statusStrip1.Refresh();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form aboutForm = new AboutBox1();
+            aboutForm.Show(this);
+        }
+    }
+}
